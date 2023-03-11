c9d46bb224cafc66eaa1d114aa7c12d68a193411
Henry Truong <henry.trb@gmail.com>
2023-03-11 18:42:32 -0500

    add route to delete urls and update views for form to delete

--- express_server.js
+++ express_server.js
@@ -45,6 +45,12 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${shortURL}`);
 });
 
+app.post("/urls/:id/delete", (req, res) => {
+  let shortURL = req.params.id;
+  delete urlDatabase[shortURL];
+  res.redirect("/urls");
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
