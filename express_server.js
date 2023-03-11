b414a37f1a7f4b062dbab186d4a774655101e8d4
Henry Truong <henry.trb@gmail.com>
2023-03-11 17:49:21 -0500

    Add generateRandomString for shortURL key. Complete POST route for new URLS. New route to redirect user if given shortURL

--- express_server.js
+++ express_server.js
@@ -16,6 +16,10 @@ app.set("view engine", "ejs");
 //MIDDLEWARE
 app.use(express.urlencoded({ extended: true }));
 
+const generateRandomString = (len) => {
+  return Math.random().toString(36).substring(2, len + 2);
+};
+
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
@@ -31,10 +35,20 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:id", (req, res) => {
+  console.log(req.params);
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
+});
+
 //POST
 app.post("/urls", (req, res) => {
   console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  let shortURL = generateRandomString(6);
+  urlDatabase[shortURL] = req.body.longURL;
+  console.log(urlDatabase);
+  // res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  res.redirect(`/urls/${shortURL}`);
 });
 
 // app.get("/urls.json", (req, res) => {
