0c0794943c43b8cf17874badd6ebc1da766881d7
Henry Truong <henry.trb@gmail.com>
2023-03-23 00:00:28 -0400

    added route for login, redirect to /urls after login

--- express_server.js
+++ express_server.js
@@ -65,6 +65,12 @@ app.post("/urls/:id/delete", (req, res) => {
   res.redirect("/urls");
 });
 
+//LOGIN
+app.post("/login", (req, res) => {
+  let username = req.body.username;
+  res.cookie("username", username);
+  res.redirect(`/urls`);
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
