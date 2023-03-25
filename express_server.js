7f7dd2c0472ad873a920ae26fb5b3f82429ab0e4
Henry Truong <henry.trb@gmail.com>
2023-03-25 00:10:09 -0400

    added route for logout and added logout button for header partial

--- express_server.js
+++ express_server.js
@@ -80,6 +80,12 @@ app.post("/login", (req, res) => {
   res.redirect(`/urls`);
 });
 
+//LOGOUT
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect(`/urls`);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
